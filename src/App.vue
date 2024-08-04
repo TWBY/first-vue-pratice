de036619b562637a31702956552af4c01eb7c625
阮柏燁 <willy633526@gmail.com>
2024-08-04 16:50:22 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>計數器: {{ counter }}</p>
+    <button @click="incrementCounter">增加</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Vue 3 簡單範例')
+const counter = ref(0)
+
+function incrementCounter() {
+  counter.value++
+}
+</script>
+
+<style scoped>
+/* 您可以在這裡添加任何組件特定的樣式 */
+</style>
